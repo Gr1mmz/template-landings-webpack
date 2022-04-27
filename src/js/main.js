8827682e702aa602a55eca37861efe1173c318f0
Mikhail <admin@kraevskiy-web.ru>
2022-04-27 15:04:27 +0800

    end base webpack settings

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import '../scss/main.scss';
+import '../index.html';
+
+const user = {
+  firstName: 'Mikhail',
+  age: 27
+};
+
+const newUser = {...user};
+
+console.log(user);
+
---
+++ webpack.config.js
@@ -0,0 +1,124 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {defaultMinimizerOptions} = require('html-loader');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const srcFolder = 'src';
+const buildFolder = 'dist';
+
+const paths = {
+  src: path.resolve(srcFolder),
+  build: path.resolve(buildFolder)
+}
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+const mode = isDev ? 'development' : 'production';
+console.log(mode + ' mode');
+
+module.exports = {
+  context: path.resolve(__dirname, paths.src),
+  mode: mode,
+  entry: './js/main.js',
+  output: {
+    path: path.resolve(__dirname, paths.build),
+    filename: `./js/${filename('js')}`,
+    publicPath: '',
+    clean: true,
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: buildFolder,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  devtool: 'inline-source-map',
+  optimization: {
+    splitChunks: {chunks: 'all'},
+    minimize: isProd,
+    minimizer: [new TerserPlugin()]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, `${srcFolder}/index.html`),
+      filename: 'index.html',
+      minify: isProd
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: `./img`, to: `img`,
+          noErrorOnMissing: false,
+          force: true
+        },
+        {
+          from: `./assets`, to: `./`,
+          noErrorOnMissing: true,
+          force: true
+        },
+        {
+          from: `./favicon.ico`, to: `./`,
+          noErrorOnMissing: true
+        }
+      ],
+    }),
+    new MiniCssExtractPlugin({filename: `./css/${filename('css')}`})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          minimize: {
+            ...defaultMinimizerOptions,
+            removeComments: true,
+            collapseWhitespace: true,
+          },
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 0,
+              sourceMap: false,
+              modules: false,
+              url: {
+                filter: (url, resourcePath) => {
+                  if (url.includes("img") || url.includes("fonts")) {
+                    return false;
+                  }
+                  return true;
+                },
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                outputStyle: "expanded",
+              },
+            }
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+}
